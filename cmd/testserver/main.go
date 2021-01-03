67d16486b1fc4c09676934ff634a021d74b015e9
Sneha Inguva <snehainguva@Snehas-MacBook-Pro-2.local>
2021-01-03 18:06:37 -0500

    Enabling cpu amd memory profiling

--- cmd/testserver/main.go
+++ cmd/testserver/main.go
@@ -6,6 +6,11 @@ import (
 	"io"
 	"log"
 	"net/http"
+	/* Adding this http endpoint to download live profiles */
+	_ "net/http/pprof"
+	"os"
+	"runtime"
+	"runtime/pprof"
 )
 
 type helloHandler struct {
@@ -24,21 +29,50 @@ func (*healthHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "okay!")
 }
 
-var addrFlag int
+var addrFlag = flag.Int("addrFlag", 8080, "address on which to serve app")
+
+// Note: These flags were added to consider cases of cpu or memory profiling
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
+var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
 
 func main() {
 
 	/* Validate that the port provided is legit */
-	flag.IntVar(&addrFlag, "addrFlag", 8080, "address on which to serve app")
 	flag.Parse()
 
-	if addrFlag < 1 || addrFlag > 65535 {
+	if *addrFlag < 1 || *addrFlag > 65535 {
 		log.Fatal("provided flag must be between values of 1 and 65535")
+	}
+
+	// Below added to enable cpu profiling
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		defer f.Close() // error handling omitted for example
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
 	}
 
 	mux := http.NewServeMux()
 	mux.Handle("/", &helloHandler{})
 	mux.Handle("/health", &healthHandler{})
 
-	http.ListenAndServe(fmt.Sprintf(":%d", addrFlag), mux)
+	http.ListenAndServe(fmt.Sprintf(":%d", *addrFlag), mux)
+
+	// Below added to enable memory profiling
+	if *memprofile != "" {
+		f, err := os.Create(*memprofile)
+		if err != nil {
+			log.Fatal("could not create memory profile: ", err)
+		}
+		defer f.Close() // error handling omitted for example
+		runtime.GC()    // get up-to-date statistics
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			log.Fatal("could not write memory profile: ", err)
+		}
+	}
 }
